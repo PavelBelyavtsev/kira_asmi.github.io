5924d68b9cfcad32d42f8d3547926799a3501b5d
PavelBelyavtsev <62264744+kira-asmi@users.noreply.github.com>
2021-01-23 02:17:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+/*if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
   console.log('this is a touch device');
 } else {
   console.log('this is not a touch device');
   document.body.classList.add('no-touch');
-}
+}*/
